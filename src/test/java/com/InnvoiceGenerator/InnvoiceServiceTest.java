4c2346050eb4586d5de381b7d860797c00fb9a7e
DipaliBachhav <dipa2718@gmail.com>
2020-05-29 12:31:55 +0530

    Added Feature To Calculate Minimum Total Fare

--- src/test/java/com/InnvoiceGenerator/InnvoiceServiceTest.java
+++ src/test/java/com/InnvoiceGenerator/InnvoiceServiceTest.java
@@ -6,10 +6,19 @@ import org.junit.Test;
 public class InnvoiceServiceTest {
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceServiceGenerator invoiceService = new InvoiceServiceGenerator();
+        InvoiceServiceGenerator invoiceServiceGenerator = new InvoiceServiceGenerator();
         double distance = 2.0;
         int time = 5;
-        double fare = invoiceService.calculateFare(distance, time);
+        double fare = invoiceServiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        InvoiceServiceGenerator invoiceServiceGenerator = new InvoiceServiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceServiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
     }
 }
